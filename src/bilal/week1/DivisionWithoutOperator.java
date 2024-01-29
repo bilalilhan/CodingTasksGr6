16f0634b853182eac11fc2c92f72f60e8ae70216
bilal <bilalilhan23@gmail.com>
2024-01-29 13:21:05 -0700

    week 1 done

---
+++ src/bilal/week1/DivisionWithoutOperator.java
@@ -0,0 +1,10 @@
+package bilal.week1;
+
+public class DivisionWithoutOperator {
+    //Write a method that can divide two numbers without using division
+
+    public static void main(String[] args) {
+
+    }
+
+}
