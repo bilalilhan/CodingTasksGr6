2cdb95a5fdabb354eb02ad4776ec73b9371150d7
Burcu32 <bondergayipov@gmail.com>
2024-01-29 13:41:04 -0700

    week 1 created

---
+++ src/burcu/week1/DivisionWithoutOperator.java
@@ -0,0 +1,9 @@
+package burcu.week1;
+
+public class DivisionWithoutOperator {
+    //Write a method that can divide two numbers without using division operator
+    public static void main(String[] args) {
+
+    }
+
+}
