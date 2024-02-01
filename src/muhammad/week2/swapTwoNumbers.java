19d9c0f9e16b9f052c99e665d37cd39aa7e19908
muhammad <muhachmax@gmail.com>
2024-02-01 14:06:07 -0800

    Added one more sentence for practice

--- src/muhammad/week2/swapTwoNumbers.java
+++ src/muhammad/week2/swapTwoNumbers.java
@@ -3,5 +3,6 @@ public class swapTwoNumbers {
     //Swap two variable' values without using a third variable
     public static void main(String[] args) {
 // preparing to solve this task
+        // added one more
     }
 }
