bccdbcfce8d099b27a80610206896ef4a34699cb
bilal <bilalilhan23@gmail.com>
2024-02-01 01:09:58 -0700

    Print Consecutive Numbers class created

--- src/bilal/week2/printConsecutiveNumbers.java
+++ src/bilal/week2/printConsecutiveNumbers.java
@@ -30,6 +30,60 @@ public class printConsecutiveNumbers {
     public static void main(String[] args) {
 
 
+        // consecutiveNumbersWords(-30);  // Invalid entry: -30. Please try with positive numbers!
+        // consecutiveNumbersWords(0);    // Invalid entry: 0. Please try with positive numbers!
+
+
+        consecutiveNumbersWords(60);   //...expected result
+
     }
 
+
+
+    public static void consecutiveNumbersWords (int number) {
+
+        if (number <=0 ) {  // Restriction for 0 and negative numbers
+            System.err.println("Invalid entry: " + number +". Please try with positive numbers!");
+            System.exit(1);
+        }
+
+
+        for (int i = 1; i <= number ; i++) {
+
+            if ( i % 30 == 0) { // Start with divisible by MAX >>> 2,3,and 5 >>> means divisible by 30
+                System.out.println("CodilityTestCoders");
+                continue; // Execute "CodilityTestCoders" and SKIP this number
+
+            } else if (i%15==0 && !( i % 2 == 0)) { // continue with divisible by SECOND MAX >> 3 and 5  >>>means divisible by 15
+                System.out.println("TestCoders");
+                continue;
+
+            } else if (i%10==0  && !(i%3==0) ) { // continue with divisible by THIRD MAX >> 2 and 5  >>>means divisible by 10
+                System.out.println("CodilityCoders");
+                continue;
+
+            } else if (i%6==0 && !(i%5==0 )) { // .......
+                System.out.println("CodilityTest");
+                continue;
+
+            } else if (i%5==0 && !( i%3==0 && i%2==0)) { //.....
+                System.out.println("Coders");
+                continue;
+
+            } else if (i%3==0 && !(i%2==0 && i%5==0)) {  // .....
+                System.out.println("Test");
+                continue;
+
+            } else if (i%2==0 && !(i%3==0 && i%5==0)) {  //.....
+                System.out.println("Codility");
+                continue;
+            }
+
+            System.out.println(i);
+        }
+
+    }
 }
+
+
+
