cb264a5a3b667b7e0846fe4e833ae3a9ae3d7ddf
Filora <filorat@outlook.com>
2024-02-02 17:09:41 -0700

    Finra solution added

--- src/filora/week1/Finra.java
+++ src/filora/week1/Finra.java
@@ -7,6 +7,24 @@ public class Finra {
     //for numbers which are a multiple of both 3 and 5, print "FINRA" instead of the number.
    public static void main(String[] args){
 
-   }
 
-}
+FINRA();
+
+   }
+   public static void FINRA(){
+      String result = " ";
+      for(int i = 3;i <= 30; i++){
+         if ( i % 5==0 && i % 3==0) //multiple of 3 & 5
+            result += "FINRA ";
+         else if(i%5==0) // multiple of 5
+            result += "RA ";
+            else if(i % 3==0) //Multiple of 3
+               result+="FIN ";
+            else
+               result += i+ " ";
+            }
+         System.out.println(result);
+
+         }
+      }
+
