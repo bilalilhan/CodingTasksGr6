f7e7401e436acec00ad5d13538b0aca325f77668
Rubinah nikzad <moqads.nikzad2021@gamil.com>
2024-02-04 14:58:12 -0800

    set up

--- src/Rubinah/week1/DivisionWithoutOperator.java
+++ src/Rubinah/week1/DivisionWithoutOperator.java
@@ -4,6 +4,6 @@ public class DivisionWithoutOperator {
     //Write a method that can divide two numbers without using division operator
 
     public static void main(String[] args) {
-
+//I start 
     }
 }
