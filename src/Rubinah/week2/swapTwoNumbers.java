3a09979ac97480269719e34035f8b37b67e56940
Rubinah nikzad <Moqads.nikzad2021@gmail.com>
2024-02-06 19:54:15 -0800

    created swap

--- src/Rubinah/week2/swapTwoNumbers.java
+++ src/Rubinah/week2/swapTwoNumbers.java
@@ -1,8 +1,15 @@
 package Rubinah.week2;
 public class swapTwoNumbers {
-    public static void main(String[] args) {
+    public static void main(String[]args){
+        //Swap two variable values without using a third variable
+        int a =15;
+        int b =10;
+        a=a+b;   //a value + b value =25
+        b=a-b;    //a value - b value=10
+        a=a-b;     //a value - b value=25
+
+        System.out.println("A=" +a +"\n"+" B="+b);
+
 
     }
-    //Swap two variable' values without using a third variable
-
 }
