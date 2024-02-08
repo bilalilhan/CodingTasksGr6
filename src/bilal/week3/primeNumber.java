7e20f239beaee6cd65f5f9f96e39f150f02919b5
bilalilhan <bilalilhan23@gmail.com>
2024-02-07 22:48:21 -0700

    week3 classes created

---
+++ src/bilal/week3/primeNumber.java
@@ -0,0 +1,13 @@
+package bilal.week3;
+
+public class primeNumber {
+     // Write a method that can check if a number is prime or not
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+}
---
+++ src/bilal/week3/reverseNegativeNumber.java
@@ -0,0 +1,16 @@
+package bilal.week3;
+
+public class reverseNegativeNumber {
+    //Write a return method that can reverse digits of a negative number and return it as int
+    //input: -35  output: -53
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+
+}
+
