f61ae8be2a07de4358c6cc6cee0bb8b2b20cb830
Rubinah nikzad <moqads.nikzad2023@gmail.com>
2024-02-13 23:31:45 -0800

    created and set up

---
+++ src/Rubinah/week3/reverseNegativeNumber.java
@@ -0,0 +1,13 @@
+package Rubinah.week3;
+
+public class reverseNegativeNumber {
+    public static void main(String[] args) {
+        
+    }
+    //Write a return method that can reverse digits of a negative number and return it as int
+    //input: -35  output: -53
+
+
+
+}
+
