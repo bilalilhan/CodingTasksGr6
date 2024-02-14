5a6c9840d82e0a2c16a457d8abc2a7c0721623b8
Rubinah nikzad <moqads.nikzad2023@gmail.com>
2024-02-13 23:42:14 -0800

    add main method

---
+++ src/Rubinah/week4/FrequencyofCharacters.java
@@ -0,0 +1,11 @@
+package Rubinah.week4;
+
+public class FrequencyofCharacters {
+    public static void main(String[] args) {
+
+    }
+    /*Write a return method that can find the frequency of characters
+    Ex:  FrequencyOfChars("AAABBCDD") ==> A3B2C1D2*/
+
+
+}
