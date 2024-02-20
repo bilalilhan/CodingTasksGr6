6aef58a37f2c3f65feda653bada9647bfdddee26
Burcu32 <bondergayipov@gmail.com>
2024-02-20 12:56:22 -0700

    wrote a function in the printConsecutiveNumbers.java

--- src/burcu/week2/printConsecutiveNumbers.java
+++ src/burcu/week2/printConsecutiveNumbers.java
@@ -26,8 +26,32 @@ public class printConsecutiveNumbers {
         Codility
         17
 */
-         public static void main(String[] args) {
+             public static void main(String[] args) {
+                 printConsecutiveNumbers(24);
+             }
 
+             public static void printConsecutiveNumbers(int N) {
+                 for (int i = 1; i <= N; i++) {
+                     if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0) {
+                         System.out.println("CodilityTestCoders");
+                     } else if (i % 2 == 0 && i % 3 == 0) {
+                         System.out.println("CodilityTest");
+                     } else if (i % 2 == 0 && i % 5 == 0) {
+                         System.out.println("CodilityCoders");
+                     } else if (i % 3 == 0 && i % 5 == 0) {
+                         System.out.println("TestCoders");
+                     } else if (i % 2 == 0) {
+                         System.out.println("Codility");
+                     } else if (i % 3 == 0) {
+                         System.out.println("Test");
+                     } else if (i % 5 == 0) {
+                         System.out.println("Coders");
+                     } else {
+                         System.out.println(i);
+                     }
+                 }
+             }
          }
-}
+
+
 
