6ab986b603b8e331af845bdf6e05ba360ab7d454
Burcu32 <bondergayipov@gmail.com>
2024-02-21 00:20:04 -0700

    added solution

--- src/burcu/week2/swapTwoNumbers.java
+++ src/burcu/week2/swapTwoNumbers.java
@@ -3,5 +3,18 @@ public class swapTwoNumbers {
     //Swap two variable' values without using a third variable
     public static void main(String[] args) {
 
-    }
-}
+                int a = 10;
+                int b = 15;
+
+                // Swapping
+                a = a + b;
+                b = a - b;
+                a = a - b;
+
+                System.out.println("After swapping:");
+                System.out.println("a = " + a);
+                System.out.println("b = " + b);
+            }
+        }
+
+
