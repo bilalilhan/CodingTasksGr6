f4b1252ab89392b5ba3da957c1e57a46eb1baaa3
Iroda <adori7775@gmail.com>
2024-02-23 09:30:14 -0600

    Week 3 file changed

--- src/iroda/week3/reverseNegativeNumber.java
+++ src/iroda/week3/reverseNegativeNumber.java
@@ -12,25 +12,7 @@ public class reverseNegativeNumber {
     //input: -35  output: -53
 
 
-    public static int reverseNegativeNumber(int number) {
-        boolean isNegative = number < 0 ? true : false;
-        if (isNegative) {
-            number = number * -1;
-        }
-        int reverse = 0;
-        if (isNegative) {
-            number = reverse * -1;
-
-        }
-        return reverse;
-    }
-
-    public static void main(String[] args) {
-
-        Scanner sc =  new Scanner(System.in);
-        System.out.println("Enter a number");
-        int number = sc.nextInt();
 
     }
-}
+
 
