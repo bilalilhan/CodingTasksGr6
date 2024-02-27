8047b18b0b370699ee805961db560cee855c79b5
Iroda <adori7775@gmail.com>
2024-02-26 21:43:22 -0600

    Week 5 solved

--- src/iroda/week5/findUnique.java
+++ src/iroda/week5/findUnique.java
@@ -3,6 +3,29 @@ package iroda.week5;
 public class findUnique {
     /*Write a return  method that can find the unique characters from the String
       Ex:  unique("AAABBBCCCDEF")  ==>  "DEF";*/
+    public static void main(String[] args) {
 
+        String str = "AAABBBCCCDEF";
+
+        for (int i = 0; i < str.length(); i++) {
+            int num = 0;
+            for (int j = 0; j < str.length(); j++) {
+
+                if (str.charAt(i) == str.charAt(j) && i != j) {//checking if 2 characters are equal
+                    num = 1;
+                    break;
+                }
+            }
+            if (num == 0) {
+                System.out.println(str.charAt(i));
+
+            }
+        }
+
+
+
+
+
+    }
 
 }
--- src/iroda/week5/reverse.java
+++ src/iroda/week5/reverse.java
@@ -5,4 +5,20 @@ public class reverse {
     Ex: Reverse("ABCD"); ==> DCBA*/
 
 //solution
+
+public static void main(String[] args) {
+
+    String str1 = "ABCD";
+
+    String str2 ="";
+
+    char ch;
+
+    System.out.println("Original word: " + str1);
+    for (int i = 0; i < str1.length(); i++) {
+        ch = str1.charAt(i);//extract each character
+        str2 = ch + str2;//add each character to the existing string
+    }
+    System.out.println("Reversed word: " + str2);
+}
 }
