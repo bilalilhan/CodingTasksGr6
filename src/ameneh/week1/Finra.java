d7ff61072a58214c9ba45284d3619ba598a738a0
ameneh <72556106+amenehmousavi@users.noreply.github.com>
2024-02-27 17:54:05 -0800

    Ameneh week1

--- src/ameneh/week1/Finra.java
+++ src/ameneh/week1/Finra.java
@@ -6,4 +6,34 @@ public class Finra {
     //for numbers which are a multiple of 5, print "RA" instead of the number.
     //for numbers which are a multiple of both 3 and 5, print "FINRA" instead of the number.
 
+    public static void main(String[] args) {
+        printFinra();
+    }
+
+    public static void printFinra() {
+        String result = " - ";
+        //After printing each value (number, "FIN", "RA", or "FINRA"),
+        // the code appends the string "-" using System.out.print(result);.
+
+        for (int i = 1; i <= 30; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {  // Check if the number is a multiple of both 3 and 5
+                System.out.print( "FINRA" ); // If true, print "FINRA"
+
+            } else if (i % 3 == 0) {        // If not, check if the number is a multiple of 3
+                System.out.print( "FIN" ); // If true, print "FIN"
+
+            } else if (i % 5 == 0) {        // If not, check if the number is a multiple of 5
+                System.out.print( "RA" );  // If true, print "RA"
+
+            } else {
+                System.out.print( i );  // If the number is not a multiple of 3 or 5, print the number itself
+            }
+
+            if (i < 30) {
+                System.out.print( result );
+            }
+             // Checks if the current value of i is less than 30.
+            // If the condition is true, prints the value of the result variable, which is "-".
+        }
+    }
 }
--- src/ameneh/week1/OddOrEven.java
+++ src/ameneh/week1/OddOrEven.java
@@ -1,8 +1,26 @@
 package ameneh.week1;
 
 public class OddOrEven {
-    /*Write  a method which can identify given number is even or odd
+    /*Write a method which can identify given number is even or odd
     Output ex:
     identify(5); ->"Odd"
     identify(6); ->"Even"*/
+
+    public static void main(String[] args) {
+        // Example usage:
+        identify(5); // Output: Odd
+        identify(6); // Output: Even
+    }
+
+    public static String identify(int number) {
+        //The identify method is declared. It takes an integer parameter named number and returns a string.
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even");
+            return "Even";
+        } else {
+            System.out.println(number + " is Odd");
+            return "Odd";
+        }
+        //Inside the identify method, there is an if-else statement to check if the given number is even or odd based on the remainder when divided by 2.
+    }
 }
