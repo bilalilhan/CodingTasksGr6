ed9060f2a13fdf967d5422d656d0844a858bae86
Burcu32 <bondergayipov@gmail.com>
2024-02-28 15:15:36 -0700

    added files

---
+++ src/burcu/week4/RemoveDuplicates.java
@@ -0,0 +1,8 @@
+package burcu.week4;
+
+public class RemoveDuplicates {
+    /*Write a return method that can remove the duplicated values from String
+        Ex:  removeDup("AAABBBCCC")  ==> ABC*/
+
+
+}
