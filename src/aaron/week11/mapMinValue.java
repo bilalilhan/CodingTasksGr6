9d50084b5b9662437b8c6890429fad3b39378579
Aaron <aaron@cybertekschool.com>
2024-04-11 17:04:57 +0300

    Week 11 Solutions

---
+++ src/aaron/week11/mapMinValue.java
@@ -0,0 +1,29 @@
+package aaron.week11;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class mapMinValue {
+//Write a method that can return the minimum value from a map (DO NOT use sort method)
+
+    public static void main(String[] args) {
+        Map<String, Integer> test = new HashMap<>();
+        test.put("Aaron", 11);
+        test.put("Harun", 5);
+        test.put("Cydeo", 27);
+        test.put("Java", 100);
+        System.out.println(minValue(test));
+    }
+
+
+    public static int minValue(Map<String, Integer> map) {
+        SortedSet<Integer> sortedMapValues = new TreeSet<>(map.values());
+        return sortedMapValues.first();
+    }
+
+
+
+}
