0891eb4acd04db1c6a5097892c3ccde53148bb01
filora <filorat@outlook.com>
2024-04-15 01:32:02 -0600

    task11 - MapMinValue task solution

--- src/filora/week11/mapMinValue.java
+++ src/filora/week11/mapMinValue.java
@@ -1,11 +1,19 @@
 package filora.week11;
 
 
+import java.util.*;
+
 public class mapMinValue {
 //Write a method that can return the minimum value from a map (DO NOT use sort method)
 
+   public static void main(String[] args) {
+      Map<String, Integer> scores = new HashMap<>();
+      scores.put("Alice", 88);
+      scores.put("Bob", 95);
+      scores.put("Charlie", 82);
+      scores.put("Diana", 95);
 
-
-
-
+      int minValue = Collections.min(scores.values());
+      System.out.println("The minimum score is: " + minValue);
+   }
 }
